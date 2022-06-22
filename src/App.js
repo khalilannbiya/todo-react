3c0808ee39dcd3e5f8f0ed288bd7aeb716493448
khalilannbiya <syeichkhalil@gmail.com>
2022-06-23 06:54:20 +0700

    membuat rancangan component form, dan card todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const [todoList, setTodoList] = useState([]);
+   const [form, setForm] = useState({
+      todo: "",
+   });
+
+   const handleChange = (e) => {
+      console.log(e.target.value);
+      setForm({
+         todo: e.target.value,
+      });
+   };
+
+   return (
+      <div>
+         <div className="jumbotron">
+            <h1>Todo List App</h1>
+            <p>{form.todo}</p>
+            <form action="" className="form">
+               <input type="text" name="todo" onChange={handleChange} placeholder="Todo" />
+               <button type="submit" className="btn-submit">
+                  Submit
+               </button>
+            </form>
+         </div>
+         <div className="content">
+            <div className="card">
+               <div className="action">
+                  <input type="checkbox" name="" id="" />
+               </div>
+               <div className="text">Todo here</div>
+            </div>
+         </div>
+      </div>
+   );
 }
 
 export default App;
