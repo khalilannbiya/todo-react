cf0fe7ed66db2a1c1c79dd50e1716796903ffc2d
khalilannbiya <syeichkhalil@gmail.com>
2022-06-23 09:27:20 +0700

    done

--- src/App.js
+++ src/App.js
@@ -5,25 +5,73 @@ function App() {
    const [todoList, setTodoList] = useState([]);
    const [form, setForm] = useState({
       todo: "",
+      status: false,
    });
 
    const resetInput = () => {
       setForm({
          todo: "",
+         status: false,
       });
    };
 
    const handleChange = (e) => {
       console.log(e.target.value);
       setForm({
+         ...form,
          todo: e.target.value,
+         status: false,
       });
    };
 
    const handleSubmit = (e) => {
       e.preventDefault();
-      setTodoList([...todoList, form]);
+      if (form.index || form.index === 0) {
+         //Update
+         const newTodo = todoList.map((e, i) => {
+            if (i === form.index) {
+               return form;
+            } else {
+               return e;
+            }
+         });
+         setTodoList(newTodo);
+      } else {
+         //insert
+         setTodoList([...todoList, form]);
+      }
       resetInput();
+   };
+
+   const handleCheck = (index) => {
+      const newTodo = todoList.map((e, i) => {
+         if (i === index) {
+            return {
+               todo: e.todo,
+               status: true,
+            };
+         } else {
+            return e;
+         }
+      });
+      setTodoList(newTodo);
+   };
+
+   const handleDelete = (index) => {
+      const newTodo = todoList.filter((e, i) => {
+         if (i != index) {
+            return e;
+         }
+      });
+      setTodoList(newTodo);
+   };
+
+   const handleEdit = (index) => {
+      const detailTodo = {
+         index,
+         ...todoList[index],
+      };
+      setForm(detailTodo);
    };
 
    return (
@@ -41,9 +89,17 @@ function App() {
             {todoList.map((e, i) => (
                <div key={i} className="card">
                   <div className="action">
-                     <input type="checkbox" name="" id="" />
+                     <input type="checkbox" checked={e.status ? true : false} onChange={() => handleCheck(i)} />
                   </div>
                   <div className="text">{e.todo}</div>
+                  <div className="button-action">
+                     <button onClick={() => handleEdit(i)} className="btn-edit">
+                        Edit
+                     </button>
+                     <button onClick={() => handleDelete(i)} className="btn-delete">
+                        Delete
+                     </button>
+                  </div>
                </div>
             ))}
          </div>
